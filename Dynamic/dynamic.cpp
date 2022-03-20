d676094f7ec442816ba4fca95fdd7987cb685db3
steph45607 <staniswinata.stephanie@gmail.com>
2022-03-20 14:58:53 +0700

    Done Dynamic

--- Dynamic/dynamic.cpp
+++ Dynamic/dynamic.cpp
@@ -3,5 +3,17 @@
 using namespace std;
 
 int main(){
-    
+    int size;
+
+    cout << "Enter list size: ";
+    cin >> size;
+
+    DynamicList grade(size);
+    grade.checkEmpty();
+    grade.count();
+    grade.deleteAtIndex(3);
+    grade.replaceIndex(3,46);
+    grade.insertAtIndex(2,521);
+    grade.searchData(46);
+    grade.printList();
 }
--- Dynamic/dynamic.h
+++ Dynamic/dynamic.h
@@ -15,13 +15,69 @@ class DynamicList{
             arr = new int[size];
             last = -1;
         }
-        checkEmpty(){
+        void checkEmpty(){
             if(last == -1){
-                return true;
+                cout << "List is empty" << endl;
             }
-            return false;
+            else{
+                cout << "List is not empty" << endl;
+            }
         }
-        
+        void increaseSize(int *arr, int size){
+            int *arrTemp = new int[2*size];
+            for(int i = 0; i <size; i++){
+                arrTemp[i] = arr[i];
+            }
+            delete [] arr;
+            arr = arrTemp;
+        }
+        void insertAtIndex(int index, int num){
+            if(index > size){
+                increaseSize(arr, size);
+            }
+            arr[index] = num;
+            last++;
+        }
+        void searchData(int num){
+            int i;
+            bool found = false;
+            for(i = 0; i < size; i++){
+                if(arr[i] == num){
+                    found = true;
+                    break;
+                }
+            }
+            if(found){
+                cout<< "Data found at index " << i << endl;
+            }
+            else{
+                cout << "Data not found in this list." << endl;
+            }
+            
+        }
+        void deleteAtIndex(int index){
+            for(int i = index; i < last; i++){
+                arr[i] = arr[i+1];
+            }
+            last--;
+        }
+        void replaceIndex(int index, int num){
+            arr[index] = num;
+        }
+        void count(){
+            int count = 0;
+            for(int i = 0; i <= last; i++){
+                count++;
+            }
+            cout << "The size of this list is "<< count << endl;
+
+        }
+        void printList(){
+            for(int i = 0; i < size; i++){
+                cout << arr[i] << " ";
+            }
+            cout << endl;
+        }
 };
 
 
